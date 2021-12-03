6361f220ea4c5d5af4b8cf136934ee9428257788
Murillo Jorge Nobrega de Melo <murillo.jorge@totvs.com.br>
2021-12-03 13:37:17 -0300

    corrigindo o corpo do metodo post

--- src/main/java/com/crud/model/Cliente.java
+++ src/main/java/com/crud/model/Cliente.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Cliente {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
 	@Column

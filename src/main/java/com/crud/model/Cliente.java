2f94d641a030832299a597b0b139fb71df056dc9
Murillo Jorge Nobrega de Melo <murillo.jorge@totvs.com.br>
2021-12-03 13:42:10 -0300

    corrigindo o corpo do metodo post

--- src/main/java/com/crud/model/Cliente.java
+++ src/main/java/com/crud/model/Cliente.java
@@ -3,8 +3,9 @@ package com.crud.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Data;
 
@@ -14,7 +15,8 @@ import lombok.Data;
 public class Cliente {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	
 	@Column
